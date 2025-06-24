fb5e27ff59f2984933cd535228a7ed747177fdf5
svrem <svrem@icloud.com>
2025-06-24 15:37:02 +0200

    refactor: seperate answer layout from App.tsx

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,11 +1,9 @@
 import '@/App.css';
 import GameTimer from '@/components/GameTimer';
-import { cn } from '@/utils/utils';
 import 'react-circular-progressbar/dist/styles.css';
-import { useGame } from './hooks/useGame';
-import QuestionDisplay from './components/QuestionDisplay';
-
-const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+import { useGame } from '@/hooks/useGame';
+import QuestionDisplay from '@/components/QuestionDisplay';
+import AnswersDisplay from '@/components/AnswersDisplay';
 
 function App() {
   const {
@@ -38,32 +36,13 @@ function App() {
         />
       </div>
 
-      <div className='grid w-full grid-rows-4 gap-3 md:gap-5'>
-        {answers.map((answer, index) => {
-          return (
-            <button
-              key={index}
-              className={cn(
-                'btn btn-primary answer-button relative select-none rounded-2xl bg-answer-button-inactive px-20 text-[2dvh] font-bold transition-all hover:bg-answer-button-hover md:rounded-3xl md:py-10',
-                selectedAnswerIndex === index ? 'selected' : '',
-                correctAnswerIndex !== null &&
-                  (correctAnswerIndex === index ? 'correct' : 'incorrect'),
-              )}
-              onClick={() => {
-                setSelectedAnswerIndex(index);
-              }}
-              ref={selectedAnswerIndex === index ? selectedOptionRef : null}
-            >
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: ALPHABET[index] + ') ' + answer,
-                }}
-                className='absolute bottom-0 left-0 right-0 top-0 m-auto h-fit w-fit font-bold'
-              ></p>
-            </button>
-          );
-        })}
-      </div>
+      <AnswersDisplay
+        answers={answers}
+        correctAnswerIndex={correctAnswerIndex}
+        selectedAnswerIndex={selectedAnswerIndex}
+        setSelectedAnswerIndex={setSelectedAnswerIndex}
+        selectedOptionRef={selectedOptionRef}
+      />
     </div>
   );
 }
---
+++ website/src/components/AnswersDisplay.tsx
@@ -0,0 +1,76 @@
+import { cn } from '@/utils/utils';
+
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+type AnswerProps = {
+  answer: string;
+  index: number;
+  selectedAnswerIndex: number | null;
+  correctAnswerIndex: number | null;
+  setSelectedAnswerIndex: (index: number) => void;
+  selectedOptionRef: React.RefObject<HTMLButtonElement | null>;
+};
+
+function Answer({
+  answer,
+  correctAnswerIndex,
+  index,
+  selectedAnswerIndex,
+  setSelectedAnswerIndex,
+  selectedOptionRef,
+}: AnswerProps) {
+  return (
+    <button
+      className={cn(
+        'btn btn-primary answer-button relative select-none rounded-2xl bg-answer-button-inactive px-20 text-[2dvh] font-bold transition-all hover:bg-answer-button-hover md:rounded-3xl md:py-10',
+        selectedAnswerIndex === index ? 'selected' : '',
+        correctAnswerIndex !== null &&
+          (correctAnswerIndex === index ? 'correct' : 'incorrect'),
+      )}
+      onClick={() => {
+        setSelectedAnswerIndex(index);
+      }}
+      ref={selectedAnswerIndex === index ? selectedOptionRef : null}
+    >
+      <p
+        dangerouslySetInnerHTML={{
+          __html: ALPHABET[index] + ') ' + answer,
+        }}
+        className='absolute bottom-0 left-0 right-0 top-0 m-auto h-fit w-fit font-bold'
+      ></p>
+    </button>
+  );
+}
+
+type AnswersDisplayProps = {
+  answers: string[];
+  selectedAnswerIndex: number | null;
+  correctAnswerIndex: number | null;
+  setSelectedAnswerIndex: (index: number) => void;
+  selectedOptionRef: React.RefObject<HTMLButtonElement | null>;
+};
+
+export default function AnswersDisplay({
+  answers,
+  selectedAnswerIndex,
+
+  correctAnswerIndex,
+  setSelectedAnswerIndex,
+  selectedOptionRef,
+}: AnswersDisplayProps) {
+  return (
+    <div className='grid w-full grid-rows-4 gap-3 md:gap-5'>
+      {answers.map((answer, index) => (
+        <Answer
+          key={index}
+          answer={answer}
+          index={index}
+          selectedAnswerIndex={selectedAnswerIndex}
+          correctAnswerIndex={correctAnswerIndex}
+          setSelectedAnswerIndex={setSelectedAnswerIndex}
+          selectedOptionRef={selectedOptionRef}
+        />
+      ))}
+    </div>
+  );
+}
