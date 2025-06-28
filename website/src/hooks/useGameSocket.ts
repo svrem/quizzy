c7ada0069866fb4fb2657df067c2b42062845044
svrem <svrem@icloud.com>
2025-06-28 13:58:16 +0200

    refactor: update WebSocket connection

--- website/src/hooks/useGameSocket.ts
+++ website/src/hooks/useGameSocket.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 
 export function useGameSocket() {
   const socket = useMemo(() => {
-    return new WebSocket('ws://192.168.117.242:8080/ws');
+    return new WebSocket('/ws');
   }, []);
 
   return socket;
